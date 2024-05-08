e28b527dc3580e321f6cdff8eba8bcc6ec693040
Yuliya Simonchyk <medicalapp2024@gmail.com>
2024-05-08 11:54:52 +0300

    update:config vite

--- examples/foomedical/src/config.ts
+++ examples/foomedical/src/config.ts
@@ -1,5 +1,5 @@
 // Default values work on localhost:3000 and foomedical.com
 // Replace these values with your own values for production
-export const MEDPLUM_PROJECT_ID = '6cd414ee-e968-40d4-9ceb-7b884a2a7433'
-export const MEDPLUM_GOOGLE_CLIENT_ID = '371883145999-9rpko8mme3418psjprcb1qjivivj47bu.apps.googleusercontent.com';
+export const MEDPLUM_PROJECT_ID = 'be090f61-b2d7-41c9-9dbf-baf669ce28fb'
+export const MEDPLUM_GOOGLE_CLIENT_ID = '371883145999-9rpko8mme3418psjprcb1qjivivj47bu.apps.googleusercontent.comnt.com';
 export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LeFF3ApAAAAAEjqnZBVNUn860m2QwmCLNB7CFJF';
